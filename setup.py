fd0425acf36130eff1a2b472bd2338a5c78e76e7
Eduardo <eduardo@importth.is>
2020-03-04 18:34:34 -0300

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="prometeo",
-    version="1.0.0",
+    version="1.0.2",
     author="Prometeo",
     author_email="dev@prometeoapi.com",
     description="Python client library for Prometeo",
