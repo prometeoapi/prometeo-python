07b1ecc424c83d7a3123d133f5c17c538a296fb6
mrgrassho <mr.grassho@gmail.com>
2023-12-12 20:51:54 -0300

    Refactor fixtures to improve organization

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -8,8 +8,12 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.client = Client("test_key")
 
+    @property
+    def module_name(self):
+        return self.__class__.__module__.split(".")[1]
+
     def load_json(self, file_name):
-        with open("tests/fixtures/{}.json".format(file_name)) as f:
+        with open(f"tests/fixtures/{self.module_name}/{file_name}.json") as f:
             return json.load(f)
 
     def mock_get_request(self, mocker, url, fixture_name, status_code=200):
