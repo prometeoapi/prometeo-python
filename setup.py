69b2bb0f996831de7776dd07ce97464fb2bcc6d7
mrgrassho <mr.grassho@gmail.com>
2025-05-21 17:12:23 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="prometeo",
-    version="2.0.3",
+    version="2.0.4",
     author="Prometeo",
     author_email="dev@prometeoapi.com",
     description="Python client library for Prometeo",
